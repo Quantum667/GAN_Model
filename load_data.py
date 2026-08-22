3bac3133a54e20f1e92d8e9d822fdc39da7b7063
Egor Potylitsyn <egorp170200@gmail.com>
2026-08-22 11:35:24 +0300

    Fix a load_data

--- load_data.py
+++ load_data.py
@@ -4,14 +4,14 @@ from torchvision import datasets, transforms
 from config import DATA_DIR, BATCH_SIZE, NUM_WORKERS
 
 def get_data(batch_size = BATCH_SIZE):
-    transform = transforms.Compose(
+    transform = transforms.Compose([
         transforms.RandomHorizontalFlip(p=0.5),
         transforms.ToTensor(),
         transforms.Normalize(
             mean=[0.5, 0.5, 0.5],
             std=[0.5, 0.5, 0.5]
         )
-    )
+    ])
 
     dataset = datasets.CIFAR10(
         root=DATA_DIR,
